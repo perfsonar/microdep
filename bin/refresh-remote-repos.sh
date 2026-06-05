171bf3d667c32b72347068a544730bbbebecde3e
Otto J Wittner <otto.wittner@sikt.no>
2026-06-05 16:56:09 +0200

    Added checkforremote rsync

--- bin/refresh-remote-repos.sh
+++ bin/refresh-remote-repos.sh
@@ -86,12 +86,18 @@ if [  "$YESNO" != "y" -a  "$YESNO" != "Y" ]; then exit 0; fi
 # Check if sudo is avaiable on remote host
 echo "Checking remote sudo status..."
 if ! ssh $HOST which sudo > /dev/null; then
-    echo "Error: sudo not avaiable for $HOST"
+    echo "Error: sudo not available for $HOST"
     exit 1
 fi
 # Check if sudo requires password
 if ! ssh $HOST sudo true 2> /dev/null; then
     echo "Error: Passwordless sudo required on remote host, e.i. 'your_username ALL=(ALL) NOPASSWD: ALL' in /etc/sudoers.d/refresh-repo"
+    exit 1
+fi
+# Check if rsync is avaiable on remote host
+echo "Checking for rsync on remote host..."
+if ! ssh $HOST which rsync > /dev/null; then
+    echo "Error: rsync not available on $HOST"
     exit 1
 fi
 
